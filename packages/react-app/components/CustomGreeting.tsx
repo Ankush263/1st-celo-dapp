545249c06d1429e63eb9f1455376f54c4cd96bec
Ankush263 <ankushbanik263@gmail.com>
2022-08-28 16:18:53 +0530

    update

--- packages/react-app/components/CustomGreeting.tsx
+++ packages/react-app/components/CustomGreeting.tsx
@@ -1,14 +1,27 @@
 import Button from '@mui/material/Button'
 import React, { useState } from 'react'
 import TextField from '@mui/material/TextField';
+import { ethers } from "ethers";
+import contractData from "../../hardhat/artifacts/contracts/CustomGreeting.sol/CustomGreeting.json";
 
 function CustomGreeting() {
   const [greet, setGreet] = useState('')
+  const [showMsg, setMsg] = useState('')
 
-  console.log(greet)
+  const contractAddress = "0x57Af7401091Ce00167d0e99eE29F3416bcB5ac50"
+  const abi = contractData.abi
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
+  const signer = provider.getSigner()
 
-  const showGreeting = () => {
-    console.log("show greet")
+  const contract: any = contractData ? (
+    new ethers.Contract(contractAddress, abi, signer) 
+  ) : null
+
+
+  const showGreeting = async () => {
+    const msg = await contract.greet()
+    setMsg(msg)
+    console.log(msg)
   }
 
   const setGreeting = () => {
@@ -19,6 +32,7 @@ function CustomGreeting() {
     <div className='customgreeting'>
       <div className="greet">
       <Button variant="contained" onClick={showGreeting}>Greet</Button>
+      <div id="msg">{showMsg}</div>
       </div>
       <div className="set--greet">
         {/* <input type="text" name="" id="" /> */}
